3a8e4a61bb2b36abc076f3bf04536b231528b883
Liviu Ioan Iancu <liviu.iancu@tim-mb-150.3pillar.corp>
2018-02-02 00:51:19 +0200

    bind property using @Input decorator

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-
+  @Input() element: {type: string, name: string, content: string};
+  
   constructor() { }
 
   ngOnInit() {
