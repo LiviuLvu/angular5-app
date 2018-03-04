f1ea3ad54f0e6632c272d58a08dd64d441bd6e14
Liviu Ioan Iancu <liviu.iancu@tim-mb-150.3pillar.corp>
2018-03-04 10:45:02 +0200

    learning-concepts: Bind to directive properties

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,25 +1,28 @@
-import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding } from "@angular/core";
+import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding, Input } from "@angular/core";
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'red';
+  @Input() highlightColor: string = 'yellow';
+  @HostBinding('style.color') color: string;
+  
   constructor(private elRef: ElementRef ,private renderer: Renderer2){};
 
   ngOnInit() {
+    this.color = this.defaultColor;
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'green');
     this.renderer.setAttribute(this.elRef.nativeElement, 'id', 'my_custom_id', 'html');
   }
 
-  @HostBinding('style.color') color: string = 'pink';
-
   @HostListener('mouseenter') evtmouseover(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'green');
-    this.color = 'limegreen';
+    this.color = this.highlightColor;
   }
   @HostListener('mouseleave') evtmouseleave(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'lightgray');
-    this.color = 'orange';    
+    this.color = this.defaultColor;
   }
   
 }
